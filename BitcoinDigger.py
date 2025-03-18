c62122434582ae9023e32236bc89c45196675f7e
xanderx9 <olamijames99@gmail.com>
2025-03-18 18:08:19 +0100

    Update BitcoinDigger.py

--- BitcoinDigger.py
+++ BitcoinDigger.py
@@ -1,5 +1,6 @@
 # https://en.wikipedia.org/wiki/Bloom_filter
 # Bloom filter module for private key search accelerator for Bitcoin Addresses
+# To test the functionality of the script Bloom filter module is OFF !!!
 
 import Chmod
 import SpeedUP
@@ -16,15 +17,14 @@ while counter < 1:
     addr = pubtoaddr(pub)
 
     #searches file for address match if the priv key generated matchs any address in the Address.txt it will print it and auto save it
-    searchfile = open("","r")
+    searchfile = open("TestAddress.txt","r")
 
     for line in searchfile:
         if addr in line:
                 
                 print("PrivKey = "+priv+" - Address = " + addr + "\n")
-                # Autosave Private Key to file "KeyFound.txt" 
-                f = open("KeyFound.txt", 'a')
+                # Autosave Private Key to file "TestKeyFound.txt"
+                f = open("TestKeyFound.txt", 'a')
                 f.write("PrivKey = " + priv + " - Address = " + addr + "\n")
                 f.close()
-
 
